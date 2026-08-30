f675bae8d6a757859d9314a7c3f15aa74540b11b
willy <willyanalvesgomes@hotmail.com>
2026-08-30 14:35:10 -0300

    Adding while loop

--- calculator.py
+++ calculator.py
@@ -15,11 +15,42 @@ def divide(a,b):
 
 def show_menu():
     print("\n======CALCULADORA======")
-    print("1.Somar")
-    print("2.Subtrair")
+    print("1. Somar")
+    print("2. Subtrair")
     print("3. Multiplicar")
     print("4. Dividir")
     print("0. Sair")
 
 
+while True:
+    show_menu()
 
+    opcao = input("Digite uma opção: ")
+
+    if opcao == "0":
+        print("Calculadora Encerrada!")
+        break
+    if opcao not in ["1","2","3","4"]:
+        print("Opção inválida!")
+        continue
+
+    try:
+        numero1 = float(input("Digite o primeiro número: "))
+        numero2 = float(input("Digite o segundo número: "))
+    except ValueError:
+        print("Digite apenas números.")
+        continue
+
+    if opcao == "1":
+        resultado = sum(numero1, numero2)
+
+    elif opcao == "2":
+        resultado = substract(numero1, numero2)
+
+    elif opcao == "3":
+        resultado = multiply(numero1, numero2)
+
+    elif opcao == "4":
+        resultado = divide(numero1, numero2)
+
+    print(f"Resultado: {resultado}")
