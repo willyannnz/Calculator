bfed6e5c0f6a96fdccf0eb2e06825d177d39c2fb
willy <willyanalvesgomes@hotmail.com>
2026-09-01 13:08:28 -0300

    add display

--- python/gui_tkinter.py
+++ python/gui_tkinter.py
@@ -2,6 +2,15 @@ import tkinter as tk
 from core import operations
 
 window = tk.Tk()
+
+#Window title and size
 window.title("Calculadora")
 window.geometry("300x400")
+
+#This will create a display for the calculator
+display = tk.Entry(window)
+display.grid(row=0, column=0)
+
+
 window.mainloop()
+
