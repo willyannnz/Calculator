b0ba9a80c4e1152b29f17b9d6c9982d8c5f11162
willy <willyanalvesgomes@hotmail.com>
2026-09-01 11:10:29 -0300

    updating structure

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
 def sum(a,b):
     return a + b
 
-def substract(a,b):
+def subtract(a,b):
     return a - b
 
 def multiply(a,b):
@@ -12,6 +12,12 @@ def divide(a,b):
         return "Erro: Não é possivel dividir por zero(0)."
     return a / b
 
+operations = {
+    "1": sum,
+    "2": subtract,
+    "3": multiply,
+    "4": divide
+}
 
 def show_menu():
     print("\n======CALCULADORA======")
@@ -24,33 +30,26 @@ def show_menu():
 
 while True:
     show_menu()
-
     opcao = input("Digite uma opção: ")
 
     if opcao == "0":
         print("Calculadora Encerrada!")
         break
-    if opcao not in ["1","2","3","4"]:
+    if opcao not in operations:
         print("Opção inválida!")
         continue
 
     try:
-        numero1 = float(input("Digite o primeiro número: "))
-        numero2 = float(input("Digite o segundo número: "))
+        number1 = float(input("Digite o primeiro número: "))
+        number2 = float(input("Digite o segundo número: "))
     except ValueError:
         print("Digite apenas números.")
         continue
+    function = operations[opcao]
+    result = function(number1, number2)
 
-    if opcao == "1":
-        resultado = sum(numero1, numero2)
+    if result is None:
+        print("Erro: Operação inválida.")
+    else:
+        print(f"Resultado: {result}")
 
-    elif opcao == "2":
-        resultado = substract(numero1, numero2)
-
-    elif opcao == "3":
-        resultado = multiply(numero1, numero2)
-
-    elif opcao == "4":
-        resultado = divide(numero1, numero2)
-
-    print(f"Resultado: {resultado}")
