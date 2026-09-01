16835c48747d60c1cf38dab9b811d595505fcac2
willy <willyanalvesgomes@hotmail.com>
2026-09-01 16:03:01 -0300

    Add comments for understand better my code

--- python/gui_tkinter.py
+++ python/gui_tkinter.py
@@ -4,32 +4,44 @@ from core import operations
 first_number = None
 operator = None
 
+#Window creation
 window = tk.Tk()
 
 #Window title and size
 window.title("Calculadora")
 window.geometry("300x400")
 
+#This will create a grid layout for the calculator
 for coluna in range(4):
     window.columnconfigure(coluna, weight=1)
 for linha in range(5):
     window.rowconfigure(linha, weight=1)
 
 #This will create a display for the calculator
-display = tk.Entry(window, font=("Arial", 20))
+display = tk.Entry(window, font=("Arial", 30))
 display.grid(row=0, column=0, columnspan=4, sticky="nsew")
 
+#This will add numbers to the display when the buttons are pressed
 def add_number(number):
     display.insert(tk.END, str(number))
 
+#This will add the operator to the display when the buttons are pressed
 def add_operator(operation):
     global first_number, operator
 
+    #here we will get the first number and the operator
     first_number = float(display.get())
+    #here we will get the operator and store it in a global variable
     operator = operation
 
+    #here we will clear the display for the second number
     display.delete(0, tk.END)
 
+#This will create a list of numbers and operators for the calculator
+numbers = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0"]
+operators = ["+", "-", "*", "/"]
+
+#This will create a function to calculate the result of the operation
 def calculate():
     second_number = float(display.get())
 
@@ -40,9 +52,7 @@ def calculate():
     display.delete(0, tk.END)
     display.insert(tk.END, str(result))
 
-    
-numbers = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0"]
-
+#This will create the buttons numbers for the calculator
 for i, number in enumerate(numbers):
     button = tk.Button(
         window,
@@ -55,15 +65,8 @@ for i, number in enumerate(numbers):
 
     button.grid(row=line, column=column, sticky="nsew")
 
-operators = ["+", "-", "*", "/"]
 
-button_equal = tk.Button(
-    window,
-    text="=",
-    command=calculate
-)
-button_equal.grid(row=4, column=1, sticky="nsew")
-
+#This will create the operator buttons for the calculator
 for i, operation in enumerate(operators):
     button = tk.Button(
         window,
@@ -73,6 +76,13 @@ for i, operation in enumerate(operators):
 
     button.grid(row=i + 1, column=3, sticky="nsew")
 
+#This will create the equal button for the calculator
+button_equal = tk.Button(
+    window,
+    text="=",
+    command=calculate
+)
+button_equal.grid(row=4, column=1, sticky="nsew")
 
 window.mainloop()
 
