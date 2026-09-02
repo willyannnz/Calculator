9c71cba6918b5bddc1ae27e63690c15531fdd3bd
willy <willyanalvesgomes@hotmail.com>
2026-09-02 15:58:27 -0300

    putting while loop for call the functions

--- java/Calculator/src/Main.java
+++ java/Calculator/src/Main.java
@@ -1,8 +1,24 @@
+import java.util.Scanner; // class that will read data from the keyboard
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("=-=-=-=-=-Calculadora-=-=-=-=");
+        Scanner scanner = new Scanner(System.in);
         Calculator calculator = new Calculator();
-        double result = calculator.divide(8, 8);
-        System.out.println(result);
+
+        while (true){
+            System.out.println("=-=-=-=-=-Calculadora-=-=-=-=");
+            System.out.println("\n1. Soma." +
+                    "\n2. Subtração." +
+                    "\n3. Divisão." +
+                    "\n4. Multiplicação." +
+                    "\n0. Sair.");
+            System.out.println("Escolha uma opção: ");
+            String opcao = scanner.next();
+            if (opcao.equals("0")){
+                System.out.println("Encerrando...");
+                break;
+            }
+
+        }
     }
 }
