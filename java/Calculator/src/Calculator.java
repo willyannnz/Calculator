3f21711b3ee7fe6a44dad627e57eed06233f6136
willy <willyanalvesgomes@hotmail.com>
2026-09-06 13:00:54 -0300

    Refactor the Calculator class to use a map of operations and implement the Calculate method.

--- java/Calculator/src/Calculator.java
+++ java/Calculator/src/Calculator.java
@@ -1,19 +1,26 @@
-public class Calculator {
+import operations.Divide;
+import operations.Operation;
+import operations.Subtract;
+import operations.Multiply;
+import operations.Sum;
 
-    // This class have the calculator functions
-   public double sum(double n1, double n2){
-       return n1 + n2;
-   }
-   public double subtract(double n1, double n2){
-       return n1 - n2;
-   }
-   public double divide(double n1, double n2){
-       if(n2 == 0) {
-           throw new ArithmeticException("Não é possível dividir por zero.");
-       }
-       return n1 / n2;
-   }
-   public double multiply(double n1, double n2){
-       return n1 * n2;
-   }
+import java.util.HashMap;
+import java.util.Map;
+
+public class Calculator {
+    private Map<String, Operation> Operations;
+
+    public Calculator() {
+        Operations = new HashMap<>();
+        Operations.put("1", new Sum());
+        Operations.put("2", new Subtract());
+        Operations.put("3", new Divide());
+        Operations.put("4", new Multiply());
+
+    }
+
+    public double Calculate (String opcao, double a, double b){
+        Operation Operation = Operations.get(opcao);
+        return Operation.calculate(a,b);
+    }
 }
