9637199ca4bb71d36ed8c61df3f4f3ef6083ecad
willy <willyanalvesgomes@hotmail.com>
2026-09-08 18:16:45 -0300

    Add new Class GUICalculator

---
+++ java/Calculator/src/GUICalculator.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class GUICalculator {
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Calculadora");
+        window.setSize(300, 400);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
+    }
+}
